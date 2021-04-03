2c96c49d8418ccdd31603775fe98aa169b3c99f3
Docrates <wer9808@naver.com>
2021-04-03 11:35:35 +0900

    datatype

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(){
 		LinkedList<int> *p;
 		bool flag = false;
 
-		cout<<"자료구조 선택(1: Stack, Other: Linked List): ";
+		cout<<"int형 자료구조 선택(1: Stack, Other: Linked List): ";
 		cin>>mode;
 						    
 		if(mode == 1)
